8a52373e3788e004a2be1f0e79613addad337a2b
Carlos Sechi <carlos.sechi@frentecorretora.com.br>
2018-09-17 02:24:29 -0300

    start working with pwa

--- quasar.conf.js
+++ quasar.conf.js
@@ -97,34 +97,34 @@ module.exports = function (ctx) {
       // workboxPluginMode: 'InjectManifest',
       // workboxOptions: {},
       manifest: {
-        // name: 'Quasar App',
-        // short_name: 'Quasar-PWA',
-        // description: 'Best PWA App in town!',
+        name: 'Controle de Aluguel',
+        short_name: 'Aluguel',
+        description: 'Tenha controle do aluguel do seu carro, para quem trabalha com Uber/99',
         display: 'standalone',
         orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        background_color: '#000000',
+        theme_color: '#000000',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
+            'src': 'statics/icons/android-icon-144x144.png',
+            'sizes': '144x144',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'statics/icons/android-icon-192x192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
-          {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
+          // {
+          //   'src': 'statics/icons/icon-256x256.png',
+          //   'sizes': '256x256',
+          //   'type': 'image/png'
+          // },
+          // {
+          //   'src': 'statics/icons/icon-384x384.png',
+          //   'sizes': '384x384',
+          //   'type': 'image/png'
+          // },
           {
             'src': 'statics/icons/icon-512x512.png',
             'sizes': '512x512',
